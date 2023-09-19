482b2d676b8cd3527807016f33d659578c167a98
Le Tran Bao Suong <letranbaosuong@gmail.com>
2023-09-19 21:27:30 +0700

    update fragment

--- app/src/main/java/com/example/fragment/MainActivity.java
+++ app/src/main/java/com/example/fragment/MainActivity.java
@@ -1,21 +1,28 @@
 package com.example.fragment;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity implements MenuFrag.IFragmentClickListener{
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity implements MenuFrag.IFragmentClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .setReorderingAllowed(true)
+                    .add(R.id.MenuFrag, MenuFrag.class, null)
+                    .add(R.id.ContentFrag, ContentFrag.class, null)
+                    .commit();
+        }
     }
 
     @Override
     public void onMenuItemClick(int position) {
         ContentFrag content = (ContentFrag) getSupportFragmentManager().findFragmentById(R.id.ContentFrag);
-        if(content!=null)
+        if (content != null)
             content.updateContent(position);
     }
 }
